b853c0c70e62b65a6727fc02244e58fab14760cc
Aditya Garg <garg.aditya775@gmail.com>
2014-04-10 23:23:58 -0400

    Paper + Sample Java Code
    Updated Paper Name + Added small Java file to read 10k lines from the
    dataset and write in another file

---
+++ rtbdJava/src/javaCodes/ReadDBLPXML.java
@@ -0,0 +1,25 @@
+package javaCodes;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadDBLPXML {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Aditya Garg\\Documents\\GitHub\\RTBDData\\oaiharvesterdemo\\citeseerx_alldata.xml"));
+	
+		BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\Aditya Garg\\Documents\\GitHub\\RTBDData\\oaiharvesterdemo\\citeseerx_alldata_small.xml"));
+	
+		for (int i =0;i<10000; i++)
+		{
+			bw.write(br.readLine());
+			bw.newLine();
+		}
+		br.close();bw.close();
+	}
+}

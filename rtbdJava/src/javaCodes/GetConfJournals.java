6486c3c971603d4a1b6379cdc724205ed7a2db4f
Aditya Garg <garg.aditya775@gmail.com>
2014-05-02 21:25:14 -0400

    relevant records and flattening
    Extract relevant records based on the filter file. input file name coded
    as dblp.xml
    Flatten the XML records to a text file for use in map reduce, appending
    xml tags as labels

--- rtbdJava/src/javaCodes/GetConfJournals.java
+++ rtbdJava/src/javaCodes/GetConfJournals.java
@@ -2,6 +2,7 @@ package javaCodes;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,19 +14,44 @@ public class GetConfJournals {
 
 	public static void main(String[] args) throws IOException 
 	{
-		BufferedReader br;
-		BufferedWriter bw;
+		//Read the filter file, only the conferences we are interested in. Filter file name coded as dbconf.txt
+		readFile();
+		//Extract relevant records based on the filter file. input file name coded as dblp.xml
+		cleanFile();
+		//flatten the XML records to a text file for use in map reduce, appending xml tags as labels
+		flatten();
+	}
+
+	static void readFile() {
+		BufferedReader Keys = null;
+		String key;
+		try {
+			Keys = new BufferedReader(new FileReader("..\\..\\RTBDData\\dbconf.txt"));
+			ListOfKeys = new ArrayList<String>();
+			while ((key = Keys.readLine()) != null) 
+			{
+				ListOfKeys.add(key);
+			}
+			Keys.close();
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	static void cleanFile() throws IOException
+	{
 		String line = "";
 		String tag = "";
 		boolean tagOpen = false;
+		BufferedReader br;
+		BufferedWriter bw;
 		br = new BufferedReader(new FileReader("..\\..\\RTBDData\\dblp.xml"));
 		bw = new BufferedWriter(new FileWriter("..\\..\\RTBDData\\dblp_conf_journals.xml"));
 
-		readFile();
 		while ((line = br.readLine()) != null) 
 		{
-		//	if(line.contains("conf/balt"))
-		//		System.out.println();
 			if (tagOpen && line.contains("</")&& (line.contains("</"+tag)))
 			{
 				tagOpen = false;
@@ -51,26 +77,9 @@ public class GetConfJournals {
 		}
 		br.close();
 		bw.close();
-	}
 
-	static void readFile() {
-		BufferedReader Keys = null;
-		String key;
-		try {
-			Keys = new BufferedReader(new FileReader("..\\..\\RTBDData\\dbconf.txt"));
-			ListOfKeys = new ArrayList<String>();
-			while ((key = Keys.readLine()) != null) 
-			{
-				ListOfKeys.add(key);
-			}
-			Keys.close();
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
 	}
-
+	
 	static boolean SpecificConfJourn(String line) throws IOException 
 	{
 		for (int index = 0; index < ListOfKeys.size(); index++) 
@@ -81,5 +90,68 @@ public class GetConfJournals {
 			}
 		}
 		return false;
+	}
+	
+	static void flatten() throws IOException
+	{
+		BufferedReader br;
+		BufferedWriter bw;
+		br = new BufferedReader(new FileReader("..\\..\\RTBDData\\dblp_conf_journals.xml"));
+		bw = new BufferedWriter(new FileWriter("..\\..\\RTBDData\\dblp_conf_journals_flattened.txt"));
+		
+		String line = "";
+		String recordTag = "";
+		String entryTag = "";
+		String previousTag = "";
+		boolean tagOpen = false;
+		
+		while ((line = br.readLine()) != null) 
+		{
+			if(line.length()>0)
+			{
+				if (tagOpen && line.contains("</"+recordTag))
+				{
+					tagOpen = false;				
+					bw.newLine();
+				} 
+				else if (tagOpen) 
+				{	
+					String text = "";
+					if((line.charAt(0)=='<')&&(line.charAt(line.length()-1)=='>'))
+					{
+						previousTag = entryTag;
+						entryTag = line.split(">")[0].split("<")[1];
+						text = line.split(">")[1].split("<")[0] ;
+						if(!previousTag.equals(entryTag))
+							bw.write("\t"+entryTag+":"+text);
+						else
+							bw.write("|"+text);
+					}
+					else if (line.charAt(0)=='<')
+					{
+						previousTag = entryTag;
+						entryTag = line.split(">")[0].split("<")[1];
+						text = line.split(">")[1];
+						if(!previousTag.equals(entryTag))
+							bw.write("\t"+entryTag+":"+text);
+						else
+							bw.write("|"+text);
+					}
+					else if (line.charAt(line.length()-1)=='>')
+					{
+						text = line.split("<")[0]; 
+						bw.write(text);
+					}									
+				}
+				else if (line.contains("key")) 
+				{				
+						tagOpen = true;					
+						recordTag = line.split(" ")[0].split("<")[1];
+						bw.write(recordTag+"\t");	
+				}
+			}
+		}
+		br.close();
+		bw.close();
 	}
 }
